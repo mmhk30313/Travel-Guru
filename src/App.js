d08e16c6a5573ffb6efcb931abe26cb093e2f3f6
mmhk30313 <mmhk.mmhkitsforyou@gmail.com>
2020-09-20 22:54:33 +0600

    Alhumdulillah, Travel Guru is done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { createContext, useState } from 'react';
 import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import Main from './Components/Main/Main';
+import Header from './Components/Header/Header';
+import Login from './Components/Login/Login';
+import HotelPlaces from './Components/HotelPlaces/HotelPlaces';
+import HotelBook from './Components/HotelBook/HotelBook';
+import PrivateRoute from './Components/PrivateRoute/PrivateRoute';
+export const UserContext = createContext();
 function App() {
+  const [loggedInUser, setLoggedInUser] = useState({});
+  const [place,setPlace] = useState({});
+  const [origin, setOrigin] = useState("");
+  const [destination, setDestination] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App justify-content-center">
+      <UserContext.Provider value={[place,setPlace,origin, setOrigin,destination, setDestination,loggedInUser, setLoggedInUser]}>
+        <Router>
+          <Header/>
+          {/* <HotelPlaces/> */}
+          <Switch>
+            <Route path="/login">
+              <Login/>
+            </Route>
+            <Route path='/place/:currentPlace'>
+              <HotelBook/>
+            </Route>
+            <PrivateRoute path='/destination'>
+              <HotelPlaces/>
+            </PrivateRoute>
+            <Route exact path='/'>
+              <Main/>
+            </Route>
+          </Switch>
+        </Router>
+      </UserContext.Provider>
     </div>
   );
 }
